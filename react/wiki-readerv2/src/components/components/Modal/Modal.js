4cae6022ece2c60e66b900d6175928aa5336be2d
Tim <github@mailtim.de>
2023-02-28 23:13:42 +0100

    Improved style for wiki-readerv2 Modal component

--- react/wiki-readerv2/src/components/components/Modal/Modal.js
+++ react/wiki-readerv2/src/components/components/Modal/Modal.js
@@ -1,16 +1,16 @@
 import './Modal.scss';
 
 
-function Modal({children, setModalPageId}) {
-    
-  return (
-    <div className="Modal">
-        <button className="Modal__close" onClick={e => setModalPageId(null)}>✕</button>
-        <div className="Modal__children">
-            { children }
+function Modal({ children, setModalPageId }) {
+
+    return (
+        <div className="Modal">
+            <button className="Modal__close" onClick={e => setModalPageId(null)}>✕</button>
+            <div className="Modal__children">
+                {children}
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Modal
