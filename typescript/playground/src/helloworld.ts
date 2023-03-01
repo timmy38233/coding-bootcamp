c1c04b11070092608aeac998cc61192bfc7d3471
Tim <github@mailtim.de>
2023-03-01 13:21:19 +0100

    Small changes to typescript playground

--- typescript/playground/src/helloworld.ts
+++ typescript/playground/src/helloworld.ts
@@ -7,14 +7,16 @@ const numD: number = 1123;
 const str: string = 'The quick brown fox jumps over the lazy dog';
 const palindrome: string = 'racecar';
 
-// Task 1
+
+//Task 1
 function add(a: number, b: number): number {
     return a + b;
 }
 
 console.log(`Sum of ${numA} + ${numB}: ${add(numA, numB)}`);
 
-// Task 2
+
+//Task 2
 function countVowels(str: string): number {
     const vowels: string = 'aeiouäöü';
     let counter: number = 0;
@@ -30,21 +32,24 @@ function countVowels(str: string): number {
 
 console.log(`The number of vowels in the string '${str}' is ${countVowels(str)}`);
 
-// Task 3
+
+//Task 3
 function raiseToThree(num: number): number {
     return num ** 3;
 }
 
 console.log(`${numA} raised to the power of 3 is ${raiseToThree(numA)}`);
 
-// Task 4
+
+//Task 4
 function isEven(num: number): boolean {
     return num % 2 === 0;
 }
 
 console.log(`The number ${numA} is even! -> ${isEven(numA)}`);
 
-// Task 5
+
+//Task 5
 function reverseString(str: string): string {
     let reversedStr = '';
     for (let c of str) {
@@ -55,7 +60,8 @@ function reverseString(str: string): string {
 
 console.log(`The string '${str}' reversed is '${reverseString(str)}'`);
 
-// Task 6
+
+//Task 6
 function multiply(numA: number, numB: number): number {
     let result: number = 0;
     for (let i: number = 0; i < numB; i++) {
@@ -66,7 +72,8 @@ function multiply(numA: number, numB: number): number {
 
 console.log(`The product of ${numA} * ${numB} is ${multiply(numA, numB)}`);
 
-// Task 7
+
+//Task 7
 function sumAll(...nums: number[]): number {
     let result: number = 0;
     for (let n of nums) {
@@ -77,14 +84,16 @@ function sumAll(...nums: number[]): number {
 
 console.log(`The sum of the numbers ${numA} + ${numB} + ${numC} is ${sumAll(numA, numB, numC)}`);
 
-// Task 8
+
+//Task 8
 function squareRoot(num: number): number {
     return Math.sqrt(num);
 }
 
 console.log(`The square root of ${numC} is ${squareRoot(numC)}`);
 
-// Task 9
+
+//Task 9
 function isPalindrome(str: string): boolean {
     let isPalindrome: boolean = true;
     for (let i: number = 0; i < (str.length / 2) && isPalindrome; i++) {
@@ -95,7 +104,8 @@ function isPalindrome(str: string): boolean {
 
 console.log(`The string '${palindrome}' is a palindrome! -> ${isPalindrome(palindrome)}`);
 
-// Task 10
+
+//Task 10
 function fact(num: number): number {
     let result: number = 1;
     for (let i: number = 1; i <= num; i++) {
@@ -106,7 +116,8 @@ function fact(num: number): number {
 
 console.log(`The factorial of ${numC} is ${fact(numC)}`);
 
-// Task 11
+
+//Task 11
 function fib(n: number): number {
     if (n <= 2) {
         return 1;
@@ -116,7 +127,8 @@ function fib(n: number): number {
 
 console.log(`The ${numC}th fibonacci number is ${fib(numC)}`);
 
-// Task 12
+
+//Task 12
 function factorialRecursion(n: number): number {
     if (n <= 1) {
         return 1;
@@ -126,7 +138,8 @@ function factorialRecursion(n: number): number {
 
 console.log(`The factorial of ${numC} is ${factorialRecursion(numC)}`);
 
-// Task 13
+
+//Task 13
 function first(n: number, func: (a: number) => any): any {
     return func(n * 2);
 }
@@ -136,7 +149,8 @@ function second(argument: number): string {
 
 console.log(first(numB, second));
 
-// Task 14
+
+//Task 14
 function countTrailingZeroes(num: number, res: number = 0): number {
     if (num < 5) {
         return res;
@@ -147,7 +161,8 @@ function countTrailingZeroes(num: number, res: number = 0): number {
 
 console.log(`The number of trailing zeroes in ${numD}! is ${countTrailingZeroes(numD)}`);
 
-// Task 15
+
+//Task 15
 function raiseToPowerRec(n: number, e: number): number {
     if (e === 0) {
         return 1;
@@ -157,7 +172,8 @@ function raiseToPowerRec(n: number, e: number): number {
 
 console.log(`${numA} raised to the power of ${numB} is ${raiseToPowerRec(numA, numB)}`);
 
-// Task 16
+
+//Task 16
 function squareAndCallCB(num: number, fn: (a: number, b: (a: number) => any) => any) {
     return fn(num ** 2, isEven)
 }
@@ -168,7 +184,8 @@ function callSecondCallback(num: number, fn: (a: number) => boolean): boolean {
 
 console.log(`The square of ${numA} is even! -> ${squareAndCallCB(numA, callSecondCallback)}`);
 
-// Task 17
+
+//Task 17
 function reverseAndCallCB(str: string, fn: (a: string, b: (a: string) => any) => any) {
     return fn(reverseString(str), countVowels);
 }
@@ -177,4 +194,4 @@ function callAnotherCallback(str: string, fn: (a: string) => number): number {
     return fn(str);
 }
 
-console.log(`The number vowels in the reversed string of '${str}' is ${reverseAndCallCB(str, callAnotherCallback)}`);
+console.log(`The number of vowels in the reversed string of '${str}' is ${reverseAndCallCB(str, callAnotherCallback)}`);
